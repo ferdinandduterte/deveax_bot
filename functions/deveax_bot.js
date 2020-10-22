cdf0a93a3b61dd507020f81b302aa0a2238fd098
Ferdinand Duterte <ferdinandduterte199@gmail.com>
2020-10-22 02:46:39 -0800

    token added

--- functions/deveax_bot.js
+++ functions/deveax_bot.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf');
 const startAction = require('./deveax_bot/start')
 const inlineAction = require('./deveax_bot/inline')
 const bot = new Telegraf(process.env.DEVEAX_BOT_TOKEN);
-
+//token insterted on netlify environment
 bot.start(ctx => {
 return startAction(ctx)
 })
