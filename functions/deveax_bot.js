f3abd429706d7bc53c433f1f885e1fa42470e5b1
Ferdinand Duterte <ferdinandduterte199@gmail.com>
2020-10-22 01:42:03 -0800

    First commit, as usual

---
+++ functions/deveax_bot.js
@@ -0,0 +1,17 @@
+const Telegraf = require('telegraf');
+const startAction = require('./deveax_bot/start')
+const inlineAction = require('./deveax_bot/inline')
+const bot = new Telegraf(process.env.DEVEAX_BOT_TOKEN);
+
+bot.start(ctx => {
+return startAction(ctx)
+})
+
+bot.on('inline_query', (ctx) => {
+return inlineAction(ctx)
+})
+
+exports.handler = async event => {
+await bot.handleUpdate(JSON.parse(event.body));
+return { statusCode: 200, body: '' };
+}
